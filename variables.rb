096316a27d9db026e50d05506b2fb41d1ef34fc8
Reilly Miller <reillyjmiller31@gmail.com>
2023-01-03 16:44:14 -0700

    Add puts and make corrections

--- variables.rb
+++ variables.rb
@@ -1,26 +1,33 @@
-## String Variable examples
+#  String Variable examples
 
-1. name = "KlayT"
-2. screen_name = "Best11"
-3. email = "warriors@gmail.com"
+ name = "KlayT"
+ screen_name = "Best11"
+ email = "warriors@gmail.com"
+ puts name
+ puts screen_name
+ puts email
 
-## Integer Variable examples
+#  Integer Variable examples
 
-1. Klay = "11"
-2. Curry = "30"
-3. Draymond = "23"
+ total_assists = 11
+ total_points = 30
+ total_rebounds = 23
 
-## Float Variable example
-1. discount = 11.30
-2. tax = 4.20
-3. item_cost = 321.45
+#  Float Variable example
+ discount = 11.30
+ tax = 4.20
+ item_cost = 321.45
 
-## Boolean Variable examples
-1. made_shot = true
-2. missed shot = false
+#  Boolean Variable examples
+ made_shot = true
+ missed_shot = false
 
-## Reassigned Strings
+#  Reassigned Strings
 
-1. name = "Curry"
-2. screen_name = "Best30"
-3. email = "goDubs@gmail.com"
+ name = "Curry"
+ screen_name = "Best30"
+ email = "goDubs@gmail.com"
+
+ puts name
+ puts screen_name
+ puts email
