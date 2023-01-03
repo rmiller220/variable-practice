a4a9f554bb6d79f7e6685b4d81cb7c1df28006c5
Reilly Miller <reillyjmiller31@gmail.com>
2023-01-03 16:19:00 -0700

    rassign string variables and fix errors

--- variables.rb
+++ variables.rb
@@ -11,11 +11,16 @@
 3. Draymond = "23"
 
 ## Float Variable example
-1. discount = "11.30"
-2. tax = "4.20"
-3. item_cost = "321.45"
+1. discount = 11.30
+2. tax = 4.20
+3. item_cost = 321.45
 
 ## Boolean Variable examples
 1. made_shot = true
 2. missed shot = false
 
+## Reassigned Strings
+
+1. name = "Curry"
+2. screen_name = "Best30"
+3. email = "goDubs@gmail.com"
