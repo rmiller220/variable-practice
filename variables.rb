285bc7903920a74f72de952d49c90f29818a53c6
Reilly Miller <reillyjmiller31@gmail.com>
2023-01-03 16:02:43 -0700

    Make changes to all categories to correct errors

--- variables.rb
+++ variables.rb
@@ -1,19 +1,19 @@
 ## String Variable examples
 
-1. "Klay"
-2. "Best11"
-3. "warriors@gmail.com"
+1. name = "KlayT"
+2. screen_name = "Best11"
+3. email = "warriors@gmail.com"
 
 ## Integer Variable examples
 
-1. "11"
-2. "30"
-3. "23"
+1. Klay = "11"
+2. Curry = "30"
+3. Draymond = "23"
 
 ## Float Variable example
-1. "11.30"
-2. "4.20"
-3. "321.45"
+1. discount = "11.30"
+2. tax = "4.20"
+3. item_cost = "321.45"
 
 ## Boolean Variable examples
 1. made_shot = true
